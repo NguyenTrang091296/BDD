f663863454d08c0064d46cc031d929d26af1f6cd
Nguyen Trang <nguyentrang091296@gmail.com>
2020-04-07 14:06:47 +0700

    (some message stating you added .idea to ignored entries)

---
+++ features/basic.feature
@@ -0,0 +1,20 @@
+Feature: checkout process
+
+  Scenario: Checkout with 1 product
+    Given I have product name $"redCOOKIE"
+    When I go to checkout process
+    Then I should see that total number of products is $"24,00"
+
+
+  Scenario: Checkout with some product
+    Given I add to cart some products
+      | name         | category           | price  |
+      | redCOOKIE    | Events and Forms   | 24,00  |
+      | redITEM      | CCK and e-Commerce | 48,00  |
+      | redBAKERY    | Templates          | 50,00  |
+    When I go to checkout process
+    Then I check some products in cart
+      | name         | category           | price  |
+      | redCOOKIE    | Events and Forms   | 24,00  |
+      | redITEM      | CCK and e-Commerce | 48,00  |
+      | redBAKERY    | Templates          | 50,00  |
---
+++ step_definitions/steps.js
@@ -0,0 +1,79 @@
+const { I } = inject();
+// Add in your custom step files
+
+Given('I have a defined step', () => {
+  // TODO: replace with your own step
+});
+
+// you can provide RegEx to match corresponding steps
+Given('I have product name ${string}', (nameProduct) => {
+    I.amOnPage('/index.php/product');
+    I.seeElement("//a[contains(text(),'Events and Forms')]");
+    I.click("//a[contains(text(),'Events and Forms')]");
+    I.seeElement("//a[contains(text(),'" + nameProduct + "')]");
+    I.click("//a[contains(text(),'" + nameProduct + "')]");
+    I.seeElement("//span[contains(text(),'Add to cart')]");
+    I.click("//span[contains(text(),'Add to cart')]");
+    I.see("Product has been added to your cart.");
+});
+
+// or a simple string
+When('I go to checkout process', () => {
+    I.amOnPage('/index.php/cart');
+});
+
+// parameters are passed in via Cucumber expressions
+Then("I should see that total number of products is ${string}", (num) => {
+    I.see(num, '.cart_totals');
+});
+
+Then("/my order amount is ${int}/", (sum) => { // eslint-disable-line
+    I.see('Total: DKK' + sum);
+});
+
+
+Given('I add to cart some products', (table) => { // eslint-disable-line
+
+    for (const id in table.rows) {
+        if (id < 1) {
+            continue; // skip a header of a table
+        }
+
+        // go by row cells
+        const cells = table.rows[id].cells;
+
+        // take values
+        const name = cells[0].value;
+        const category = cells[1].value;
+        const price = cells[2].value;
+
+        I.amOnPage('/index.php/product');
+        I.seeElement("//a[contains(text(),'" + category + "')]");
+        I.click("//a[contains(text(),'"+ category + "')]");
+        I.seeElement("//a[contains(text(),'" + name + "')]");
+        I.click("//a[contains(text(),'" + name + "')]");
+        I.see(price);
+        I.seeElement("//span[contains(text(),'Add to cart')]");
+        I.click("//span[contains(text(),'Add to cart')]");
+        I.see("Product has been added to your cart.");
+    }
+});
+
+Then('I check some products in cart', (table) => { // eslint-disable-line
+
+    for (const id in table.rows) {
+        if (id < 1) {
+            continue; // skip a header of a table
+        }
+
+        // go by row cells
+        const cells = table.rows[id].cells;
+
+        // take values
+        const name = cells[0].value;
+        const price = cells[2].value;
+
+        I.seeElement("//a[contains(text(),'" + name + "')]");
+        I.see(price);
+    }
+});
